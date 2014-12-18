ef5c5cc33790c6d09a205cf381f61899d2b35bf7
coronado EDGAR <coronadoedgar93@gmail.com>
2014-12-18 18:05:59 -0500

    update commit
    actualización de diseño y programación

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 
+function main_resizeMe(displayHeight, displayWidth,el) {
+	console.log('entro juuu');
+	//Standard dimensions, for which the body font size is correct
+	var preferredHeight = 825;
+	var preferredWidth = 900;
+
+		if (displayHeight < preferredHeight || displayWidth < preferredWidth) {
+		var heightPercentage = (displayHeight * 100) / preferredHeight;
+		var widthPercentage = (displayWidth * 100) / preferredWidth;
+		var percentage = Math.min(heightPercentage, widthPercentage);
+		var newFontSize = percentage.toFixed(2);
+
+		el.css("font-size", newFontSize + 'px');
+		} else {
+			el.css("font-size", '100px');
+		}
+}
+
+function main_display_responsive(el){
+	for(x=0;x<el.length;x++){
+		e=el[x];
+		console.log('elemento'+x);
+		if($(window).height()<600){
+			e.css('display','none');
+		}else{
+			e.css('display','block');
+		}
+	}
+}
